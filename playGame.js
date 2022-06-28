f7c41ddb90e1f743f033e5235f6fc0415b4ac627
arianneelise <arianne@devpipeline.com>
2022-06-28 13:06:14 -0600

    added some spaces

--- playGame.js
+++ playGame.js
@@ -8,7 +8,7 @@ export class PlayGame {
     self.spinResult = null;
     self.spinToMatch = null;
     self.didWin = false;
-    self.giveChance = [0, 1, 2, 3, 4];
+    self.giveChance = [0, 1, 2, 3, 4, 5];
   }
 
   async spinSymbols() {
@@ -16,13 +16,16 @@ export class PlayGame {
     let number = "";
     const chance =
       self.giveChance[Math.floor(Math.random() * self.giveChance.length)];
+
     if (chance != 1) {
       for (let i = 0; i < 15; i++) {
         await show(100);
+
         for (let n = 1; n < 7; n++) {
           document.getElementById(`window-${n}`).innerHTML =
             arr[Math.floor(Math.random() * arr.length)];
         }
+
         if (i === 14) {
           for (let n = 1; n < 7; n++) {
             number = Math.floor(Math.random() * arr.length);
@@ -36,10 +39,12 @@ export class PlayGame {
       for (let i = 0; i < 15; i++) {
         await show(100);
         self.spinResult = self.spinToMatch + self.spinToMatch;
+
         for (let n = 1; n < 7; n++) {
           document.getElementById(`window-${n}`).innerHTML =
             arr[Math.floor(Math.random() * arr.length)];
         }
+
         if (i === 14) {
           for (let n = 1; n < 7; n++) {
             document.getElementById(`window-${n}`).innerHTML =
@@ -53,12 +58,15 @@ export class PlayGame {
   async spinMatch() {
     let match = "";
     let number = "";
+
     for (let m = 0; m < 5; m++) {
       await show(100);
+
       for (let t = 1; t < 4; t++) {
         document.getElementById(`match-${t}`).innerHTML =
           arr[Math.floor(Math.random() * arr.length)];
       }
+
       if (m === 4) {
         for (let n = 1; n < 4; n++) {
           number = Math.floor(Math.random() * arr.length);
@@ -67,17 +75,14 @@ export class PlayGame {
         }
       }
     }
+
     self.spinToMatch = match;
   }
 
   async updateAmt(win) {
     await show(2000);
-    // const chance =
-    //   self.giveChance[Math.floor(Math.random() * self.giveChance.length)];
     await win.then((res) => (self.didWin = res));
-    // if (chance === 1 && !self.didWin) {
-    //   self.didWin = true;
-    // }
+
     if (self.didWin) {
       return 15;
     } else {
@@ -87,6 +92,7 @@ export class PlayGame {
 
   async checkWin() {
     await show(2000);
+
     if (
       self.spinResult.slice(3) == self.spinToMatch ||
       self.spinResult.slice(0, 3) == self.spinToMatch
